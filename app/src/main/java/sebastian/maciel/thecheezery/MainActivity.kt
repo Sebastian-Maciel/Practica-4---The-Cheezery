3b5ec87c1a8409cf007f5712fed97f9e0064e670
Sebastián García Maciel <sebastian-maciel@outlook.com>
2021-02-21 21:20:06 -0600

    Se conectaron pantallas maquetadas

--- app/src/main/java/sebastian/maciel/thecheezery/MainActivity.kt
+++ app/src/main/java/sebastian/maciel/thecheezery/MainActivity.kt
@@ -1,11 +1,21 @@
 package sebastian.maciel.thecheezery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button:Button = findViewById<Button>(R.id.button_get_started)
+
+
+        button.setOnClickListener(){
+            var intent:Intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
